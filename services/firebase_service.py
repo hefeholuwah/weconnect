9369f33caf057a9af0ee3ecec3865be92086a16f
piintegrated <piintegratedservices1@gmail.com>
2024-10-19 17:23:13 +0100

    removed sensitive files

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("../myauth.json")
+cred = credentials.Certificate("../myauth_copy.json")
 firebase_admin.initialize_app(cred)
 
 def create_user(email: str, password: str):
